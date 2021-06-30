8c68a86031160daf2e31053e1b7c166033ba79cf
Jose Martin Rangel Espinoza <jose.rangelespinoza@mercadolibre.com.mx>
2021-06-29 19:10:59 -0500

    profiling

--- testing/main_test.go
+++ testing/main_test.go
@@ -34,12 +34,31 @@ func TestMax(t *testing.T) {
 	}{
 		{4, 2, 4},
 		{3, 2, 3},
+		{2, 5, 5},
 	}
 
 	for _, item := range tables {
 		max := GetMax(item.a, item.b)
 		if max != item.n {
 			t.Errorf("GetMax was incorrect, got %d, expected %d", max, item.n)
+		}
+	}
+}
+
+func TestFib(t *testing.T) {
+	tables := []struct {
+		a int
+		n int
+	}{
+		{1, 1},
+		{8, 21},
+		{50, 12586269025},
+	}
+
+	for _, item := range tables {
+		fib := Fibonacci(item.a)
+		if fib != item.n {
+			t.Errorf("Fibonacci was incorrect, got %d expected %d", fib, item.n)
 		}
 	}
 }
